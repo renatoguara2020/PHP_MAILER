9501c3da241e9608ff0455d67d6c0fd7f1e6e6c0
Renato Alves Soares <renatoguara2020@gmail.com>
2022-10-20 08:15:10 -0300

    ok

--- PHPMailer-master/index.php
+++ PHPMailer-master/index.php
@@ -3,3 +3,19 @@
 require_once('src/PHPMailer.php');
 require_once('src/SMTP.php');
 require_once('src/Exception.php');
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+$mail = new PHPMailer(true);
+
+try {
+
+
+}catch(Exception $e){
+
+    echo "Erro ao enviar mensagem: {mail->ErrorInfo}";
+
+
+}
