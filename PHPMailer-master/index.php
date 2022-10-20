eef05a91810d2f81fe10e5ab47a9c2a2d34ceffc
Renato Alves Soares <renatoguara2020@gmail.com>
2022-10-20 10:04:02 -0300

    email funcionando

--- PHPMailer-master/index.php
+++ PHPMailer-master/index.php
@@ -19,11 +19,11 @@
     $mail->Port = 465; // or 587
     $mail->IsHTML(true);
     $mail->Username = "renatoguara2020@gmail.com";
-    $mail->Password = "ag";
+    $mail->Password = "agc";
     $mail->SetFrom("renatoguara2020@gmail.com");
     $mail->Subject = "PHP MAILER";
-    $mail->Body = "HELLO WORLD COM RENATO ALVES SOARES COM PHP MAILER";
-    $mail->AddAddress("renatoguara2020@gmail.com");
+    $mail->Body = "HELLO WORLD COM RENATO ALVES SOARES COM PHP MAILER E NATHANZINHO";
+    $mail->AddAddress("renatoguara2019@yahoo.com");
 
      if(!$mail->Send()) {
         echo "Mailer Error: " . $mail->ErrorInfo;
