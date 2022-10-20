da33f84420da0afb3b43e69f50dd3dfbda5230d7
Renato Alves Soares <renatoguara2020@gmail.com>
2022-10-20 09:15:03 -0300

    PHP MAILER FUNCIONANDO

--- PHPMailer-master/index.php
+++ PHPMailer-master/index.php
@@ -1,21 +1,33 @@
 <?php
 
-require_once('src/PHPMailer.php');
-require_once('src/SMTP.php');
-require_once('src/Exception.php');
+  require("src/PHPMailer.php");
+  require("src/SMTP.php");
+  require('src/Exception.php');
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\SMTP;
+  use PHPMailer\PHPMailer\Exception;
 
-$mail = new PHPMailer(true);
+    $mail = new PHPMailer(true);
+    $mail->IsSMTP(); // enable SMTP
 
-try {
+    $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+    $mail->SMTPAuth = true; // authentication enabled
+    $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+    $mail->Host = "smtp.gmail.com";
+    // $email->SMTPAuth = true;
+    $mail->Port = 465; // or 587
+    $mail->IsHTML(true);
+    $mail->Username = "renatoguara2020@gmail.com";
+    $mail->Password = "ag";
+    $mail->SetFrom("renatoguara2020@gmail.com");
+    $mail->Subject = "PHP MAILER";
+    $mail->Body = "hello RENATO ALVES SOARES COM PHP MAILER";
+    $mail->AddAddress("renatoguara2020@gmal.com");
 
-
-}catch(Exception $e){
-
-    echo "Erro ao enviar mensagem: {mail->ErrorInfo}";
-
-
-}
+     if(!$mail->Send()) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+     } else {
+        echo "Message has been sent";
+     }
+?>
